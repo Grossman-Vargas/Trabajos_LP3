f4577c7a15481e48e942cd5658b3b48dfe8fdb61
Grossman <grossman.vargas@ucsm.edu.pe>
2024-08-20 20:39:33 -0500

    Modificación y mejora del ejercicio 2.

--- Práctica_01/JAVA/EJ_02.java
+++ Práctica_01/JAVA/EJ_02.java
@@ -12,17 +12,18 @@ public class EJ_02 {
 
     public static int[] arr_uni(){
         Scanner num = new Scanner(System.in);
-        int n1 = num.nextInt();
-
 
         int[] arr = new int[10];
 
-        System.out.println("Ingrese un numero: ");
+        System.out.println("Ingrese el numero posicion 0: ");
         arr[0] = num.nextInt();
 
         for (int i = 1; i < 10; i++) {
+            System.out.println("Ingrese el numero posicion " + i + ": ");
             arr[i] = num.nextInt();
             while (arr[i] <= arr[i-1]){
+                System.out.println("El numero debe ser mayor a la de la anterior posicion (" +
+                        arr[i-1] + "): ");
                 arr[i] = num.nextInt();
             }
         }
